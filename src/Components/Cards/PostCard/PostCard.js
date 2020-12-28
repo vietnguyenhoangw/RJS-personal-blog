bb8c91d693404c2aa1089012831301e002f3a83e
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2020-12-28 23:31:52 +0700

    Update layout

--- src/Components/Cards/PostCard/PostCard.js
+++ src/Components/Cards/PostCard/PostCard.js
@@ -6,11 +6,15 @@ class PostCard extends Component {
   render() {
     return (
       <div
-        className="mb-4 col-md-4 col-sm-6"
-        style={{ backgroundColor: 'green', display: "flex", flexDirection: "row", height: "50vh" }}
+        className="mb-4 col-md-4 col-sm-6 pr-0"
+        style={{ display: "flex", flexDirection: "row", width: "100vh" }}
       >
-        <div style={{ backgroundColor: 'red', flex: 1 }}>
-          <h2>Hello world</h2>
+        <div style={{background:"rgb(241, 241, 241)"}}>
+          <img width="100%" height="210px" src="https://www.guillenphoto.com/data/blog/2016/005-chronique-pourquoi-choisir-un-point-de-vue/images/why-choosing-a-good-point-of-view-in-nature-photography-amar-guillen-photographer.jpg" class="w3-hover-opacity"></img>
+          <div class="p-2">
+            <p style={{marginBottom: 0}}><b>Lorem Ipsum</b></p>
+            <p>Praesent porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+          </div>
         </div>
       </div>
     );
--- src/Components/PostList/PostList.js
+++ src/Components/PostList/PostList.js
@@ -19,13 +19,22 @@ class PostList extends Component {
     ));
   };
 
+  displayHeader() {
+    return (
+      <div style={{height: "20vh",  width: "100%", backgroundColor: 'red', marginBottom: 15}}>
+        <p>Hello</p>
+      </div>
+    )
+  };
+
   render() {
     return (
       <nav
-        class="w3-animate-right main-wrapper row ml-2 p-4"
+        class="w3-animate-right main-wrapper row"
         id="mySidebar"
-        style={{ backgroundColor: 'white', overflow: "auto" }}
+        style={{ backgroundColor: 'white', overflow: "auto", paddingLeft: 15, paddingRight: 15 }}
       >
+        {this.displayHeader()}
         {this.displayList()}
       </nav>
     );
