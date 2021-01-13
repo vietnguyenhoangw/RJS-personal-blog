a1fd6811bd81c3cb3efd19d796b9625a498f90bc
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2021-01-13 15:23:22 +0700

    Config webpack, but router still not working :bug:

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 var path = require('path')
+var hwp = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
     },
+    mode: 'development',
     module: {
         rules: [
             {
@@ -15,7 +17,25 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ]
             }
         ]
+    },
+    plugins: [
+        new hwp({ template: path.join(__dirname, './index.html') })
+    ],
+    devServer: {
+        publicPath: "/",
+        host: 'localhost',
+        port: 3003,
+        historyApiFallback: true,
+        open: true
     }
 }
